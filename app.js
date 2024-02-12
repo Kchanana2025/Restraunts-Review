39c69a571d371f125ad9573aa547e88e6ba05718
Kchanana2025 <kanikachanana2025@gmail.com>
2024-02-12 12:08:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.get('/', function (req, res) {
     res.send('<h1>Hello World!</h1>');
 });
 app.get('/restaurants', function (req, res) {
     const htmlFilePath = path.join(__dirname, 'views', 'restaurants.html');
+
     res.sendFile(htmlFilePath);//resSendFile Automatically sets the Content-Type response header field file mein dekh ke basically ab browser pe html apne aap render ho jayegi alag se btana ni padhega ki ye html hai.
 });
 app.listen(3000);
